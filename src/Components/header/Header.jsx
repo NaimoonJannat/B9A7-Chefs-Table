63383eed9aa7b0447e109df4e27a208f1c37e1aa
NaimoonJannat <naimoonjannat@gmail.com>
2024-03-16 01:56:44 +0600

    Geader nav done

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
   </div>
   <div className="navbar-end">
     <div className="flex flex-row gap-4">
-    <label className="input rounded-3xl text-[#150B2BB2] text-base input-bordered flex items-center gap-2 bg-[#150b2b29]">
+    <label className="input hidden rounded-3xl text-[#150B2BB2] text-base input-bordered lg:flex items-center gap-2 bg-[#150b2b29]">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
   <input type="text" className="grow" placeholder="Search" />
 </label>
