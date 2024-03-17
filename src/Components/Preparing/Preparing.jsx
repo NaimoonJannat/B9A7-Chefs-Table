0bf93d06420d1a56a76b0da4f8f2fed63951f804
NaimoonJannat <naimoonjannat@gmail.com>
2024-03-17 13:22:56 +0600

    Completed bonus component

--- src/Components/Preparing/Preparing.jsx
+++ src/Components/Preparing/Preparing.jsx
@@ -1,6 +1,8 @@
-import PreparingItem from "../PreparingItem/PreparingItem";
-
 const Preparing = ({preparedItem}) => {
+
+    const totalTime = preparedItem.reduce((total, item) => total + item.preparing_time, 0);
+    const totalCalories = preparedItem.reduce((total, item) => total + item.calories, 0);
+
     return (
         <div className="border-[#28282833] border-2 p-5 rounded-lg text-center">
             <h2 className="text-2xl font-semibold">Currently Cooking: {preparedItem.length} </h2>
@@ -13,11 +15,23 @@ const Preparing = ({preparedItem}) => {
         <th>Calories</th>
       </tr>
     </thead>
+    <tbody className="text-base text-[#878787] text-left">
+    {preparedItem.map((item, idx) => (
+                        <tr key={idx}>
+                            <td>{item.recipe_name}</td>
+                            <td>{item.preparing_time}</td>
+                            <td>{item.calories}</td>
+                        </tr>
+                    ))}
+                    {/* Total row */}
+                    <tr>
+                        <th>Total</th>
+                        <th>{totalTime} Minutes</th>
+                        <th>{totalCalories} Calories</th>
+                    </tr>
+    </tbody>
             </table>
-            {
-        preparedItem.map((item,idx) => <PreparingItem key={idx} 
-            PreparingItem={item}></PreparingItem>)
-    }
+            
 
         </div>
     );
