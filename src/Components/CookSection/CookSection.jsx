da18059599703689b16937beb8158d389026a5f3
NaimoonJannat <naimoonjannat@gmail.com>
2024-03-17 11:16:16 +0600

    CookedItem cards disappear when preparing fixed

--- src/Components/CookSection/CookSection.jsx
+++ src/Components/CookSection/CookSection.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import CookedItem from '../CookedItem/CookedItem'
 import Preparing from '../Preparing/Preparing';
+
 const CookSection = ({cook}) => {
     const [preparedItem, setPreparedItem] = useState([]);
     const handlePreparedItem = food =>{
@@ -8,9 +9,12 @@ const CookSection = ({cook}) => {
         const newPreparedItem = [...preparedItem,food];
         setPreparedItem(newPreparedItem);
     }
+
+    const remainingCook = cook.filter(item => !preparedItem.some(prepared => prepared.id === item.id));
+
     return (
         <div className="border-[#28282833] lg:w-1/3 p-5 rounded-lg text-center">
-            <h2 className="text-2xl font-semibold">Want To Cook: {cook.length} </h2>
+            <h2 className="text-2xl font-semibold">Want To Cook: {remainingCook.length} </h2>
             <table className="table">
                 {/* head */}
     <thead className="text-base text-[#878787] text-left">
@@ -24,7 +28,7 @@ const CookSection = ({cook}) => {
     </thead>
             </table>
             {
-        cook.map(item => <CookedItem key={item.id} handlePreparedItem={handlePreparedItem}
+        remainingCook.map(item => <CookedItem key={item.id} handlePreparedItem={handlePreparedItem}
         CookedItem={item} ></CookedItem>)
     }
     <Preparing preparedItem={preparedItem}></Preparing>
